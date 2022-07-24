c491f91d295d3806d2d05068e11edd10b82000c6
pylover <vahid.mardani@gmail.com>
2022-07-24 15:13:47 +0430

    bind function

--- examples/maybe.c
+++ examples/maybe.c
@@ -19,14 +19,20 @@ struct monad {
 
 
 void 
-resolveM(struct monad *self) {
-    self->done = true;
+runM (struct monad *self, struct monad *x) {
+    self->run(self, x);
 }
 
 
 void 
-runM (struct monad *self, struct monad *x) {
-    self->run(self, x);
+resolveM(struct monad *self) {
+    struct monad *next = self->next;
+    if ((next == NULL) || self->nothing) {
+        self->done = true;
+    }
+    else {
+        next->run(next, self);
+    }
 }
 
 
@@ -36,9 +42,85 @@ returnM (taskM task, struct monad *self) {
 }
 
 
+/** 
+  Bind two monads:
+
+  m1    m2     result
+
+  o-o   O-O    o-o-O-O 
+
+  o-o   O-O    o-o-O-O 
+        |_|        |_|
+
+  o-o   O-O    o-o-O-O
+  |_|          |_____|
+
+  o-o   O-O    o-o-O-O
+  |_|   |_|    |_____|
+
+*/
+void 
+bindM(struct monad *m1, struct monad *m2) {
+    struct monad *m1last = m1;
+    struct monad *m2last = m2;
+
+    while (true) {
+        /* Open cicuit */
+        if (m1last->next == NULL) {
+            /* m1 Last element */
+            m1last->last = false;
+            m1last->next = m2;
+            return;
+        }
+
+        /* Closed cicuit */
+        if (m1last->next == m1) {
+            /* It's a closed loop, Inserting m2 before the first element. */
+            m1last->next = m2;
+            while (true) {
+                /* m2 Last element */
+                if ((m2last->next == NULL) || (m2last->next == m2)) {
+                    m2last->last = true;
+                    m2last->next = m1;
+                    return;
+                }
+            
+                /* Navigate forward */
+                m2last = m2last->next;
+            }
+            return;
+        }
+        
+        /* Try next monad */
+        m1last = m1last->next;
+    }
+}
+
+
+/** 
+  Make monad from task and bind it to m1.
+
+  m1    task   result
+
+  o-o   O      o-o-O 
+
+  o-o   O      o-o-O
+  |_|          |___|
+
+*/
+struct monad * 
+appendM(struct monad *self, taskM task, struct monad *other) {
+    struct monad *newmonad = returnM(task, self);
+    bindM(self, newmonad);
+    return newmonad;
+}
+
+
 #define RESOLVE_M(self) resolveM((struct monad*)(self))
 #define RUN_M(self, other) runM((struct monad*)(self), (struct monad*)(other))
 #define RETURN_M(task, self) returnM((taskM)(task), (struct monad*)(self))
+#define APPEND_M(self, task, other) \
+    appendM((struct monad *)(self), (taskM)(task), (struct monad*)(other))
 
 
 // MONAD_H End
@@ -58,12 +140,12 @@ void addM(struct maybe *self, struct maybe *x) {
 
 
 int main() {
-    struct maybe m1 = {.just = 2};
-    struct maybe m2 = {.just = 3};
+    struct maybe m2 = {.run = addM, .just = 2};
+    struct maybe m1 = {.run = addM, .just = 1, .next = &m2};
+    struct maybe mz = {.just = 3};
 
-    struct monad *all = RETURN_M(addM, &m1);
+    m1.run(&m1, &mz);
 
-    RUN_M(all, &m2);
     
     printf("Result: %d\n", m1.just);
     return EXIT_SUCCESS;
